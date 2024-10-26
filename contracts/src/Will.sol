89266db1bfb0a8242434f566a65c616dabe0b6db
Jianian Zheng <jnz7276@gmail.com>
2024-10-26 13:42:18 +0100

    Update Will.sol Untested
    Now the Contract can
        Update death from either intervals or Oracle external
        Add transfer asset function, to allow beneficiaries to transfer asset when category check is passed

--- contracts/src/Will.sol
+++ contracts/src/Will.sol
@@ -15,39 +15,34 @@ contract Will {
     bool public isConfirmedDead;
     uint256 public lastAliveTimestamp;
     uint256 public DEATH_INTERVAL = 5 weeks;
-    function editDeathInterval(uint256 _deathInterval) public onlyOwner {
-        DEATH_INTERVAL = _deathInterval;
-    }
-    // Update the last alive timestamp to extend the deadline
-    function stillAlive() public onlyOwner {
-        lastAliveTimestamp = block.timestamp;
-    }
 
-    // Check if the death interval has passed
-    function checkDeath() public {
-        if (block.timestamp >= lastAliveTimestamp + DEATH_INTERVAL) {
-            isConfirmedDead = true;
-        }
-    }
     struct Beneficiary {
         address addr;
         uint256 ethAmount;
+        uint256 ethReleaseTime;
         uint256 tokenAmount;
+        uint256 tokenReleaseTime;
         uint256 nftTokenId;
-        uint256 releaseTime;
+        uint256 nftReleaseTime;
         address erc20Token;
         address nftContract;
     }
 
     Beneficiary[] public beneficiaries;
-    mapping(address => uint256) public beneficiaryIndex; // Maps address to index in beneficiaries array (+1 to avoid default 0)
+    mapping(address => uint256) public beneficiaryIndex;
 
     constructor() payable {
         owner = msg.sender;
+        lastAliveTimestamp = block.timestamp;
     }
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this");
+        _;
+    }
+
+    modifier onlyBeneficiary() {
+        require(beneficiaryIndex[msg.sender] > 0, "Only beneficiary can call this");
         _;
     }
 
@@ -56,9 +51,26 @@ contract Will {
         _;
     }
 
-    // Confirm owner's death, allowing claims to be triggered by anyone
-    // oracle port 
-    function dead() public onlyOwner {
+    // Edit death interval
+    function editDeathInterval(uint256 _deathInterval) public onlyOwner {
+        DEATH_INTERVAL = _deathInterval;
+    }
+
+    // Update lastAliveTimestamp to reset death interval countdown
+    function stillAlive() public onlyOwner {
+        lastAliveTimestamp = block.timestamp;
+    }
+
+    // Check death status based on inactivity
+    function checkDeath() public {
+        if (block.timestamp >= lastAliveTimestamp + DEATH_INTERVAL) {
+            isConfirmedDead = true;
+        }
+    }
+
+    // Confirm death directly by oracle
+    // NO RESTRICTION FOR NOWWWWWWWWW
+    function dead() public {
         isConfirmedDead = true;
     }
 
@@ -66,52 +78,53 @@ contract Will {
     function addFund(
         address _beneficiary,
         uint256 _ethAmount,
+        uint256 _ethReleaseTime,
         uint256 _tokenAmount,
+        uint256 _tokenReleaseTime,
         address _erc20Token,
         uint256 _nftTokenId,
-        address _nftContract,
-        uint256 _releaseTime
+        uint256 _nftReleaseTime,
+        address _nftContract
     ) public payable onlyOwner {
         require(_beneficiary != address(0), "Invalid beneficiary address");
 
-        // Check if the beneficiary already exists
         uint256 index = beneficiaryIndex[_beneficiary];
 
         if (index == 0) {
-            // Beneficiary does not exist, so add a new one
-            addBeneficiary(_beneficiary, _ethAmount, _tokenAmount, _erc20Token, _nftTokenId, _nftContract, _releaseTime);
+            addBeneficiary(_beneficiary, _ethAmount, _ethReleaseTime, _tokenAmount, _tokenReleaseTime, _erc20Token, _nftTokenId, _nftReleaseTime, _nftContract);
         } else {
-            // Beneficiary exists, so add more assets to their existing allocation
-            addMore(index - 1, _ethAmount, _tokenAmount, _erc20Token, _nftTokenId, _nftContract, _releaseTime);
+            addMore(index - 1, _ethAmount, _ethReleaseTime, _tokenAmount, _tokenReleaseTime, _erc20Token, _nftTokenId, _nftReleaseTime, _nftContract);
         }
     }
 
-    // Helper function to add a new beneficiary
+    // Add a new beneficiary
     function addBeneficiary(
         address _beneficiary,
         uint256 _ethAmount,
+        uint256 _ethReleaseTime,
         uint256 _tokenAmount,
+        uint256 _tokenReleaseTime,
         address _erc20Token,
         uint256 _nftTokenId,
-        address _nftContract,
-        uint256 _releaseTime
+        uint256 _nftReleaseTime,
+        address _nftContract
     ) internal {
         beneficiaries.push(
             Beneficiary({
                 addr: _beneficiary,
                 ethAmount: _ethAmount,
+                ethReleaseTime: _ethReleaseTime,
                 tokenAmount: _tokenAmount,
+                tokenReleaseTime: _tokenReleaseTime,
                 nftTokenId: _nftTokenId,
-                releaseTime: _releaseTime,
+                nftReleaseTime: _nftReleaseTime,
                 erc20Token: _erc20Token,
                 nftContract: _nftContract
             })
         );
 
-        // Map the address to the index in beneficiaries array (+1 to avoid default 0)
         beneficiaryIndex[_beneficiary] = beneficiaries.length;
 
-        // Transfer ETH and tokens if specified
         if (_ethAmount > 0) {
             require(msg.value == _ethAmount, "Incorrect ETH amount sent");
         }
@@ -122,78 +135,111 @@ contract Will {
             IERC721(_nftContract).transferFrom(msg.sender, address(this), _nftTokenId);
         }
     }
-
-    // Helper function to add more assets to an existing beneficiary
+    // Add more assets to an existing beneficiary
     function addMore(
         uint256 index,
         uint256 _ethAmount,
+        uint256 _ethReleaseTime,
         uint256 _tokenAmount,
+        uint256 _tokenReleaseTime,
         address _erc20Token,
         uint256 _nftTokenId,
-        address _nftContract,
-        uint256 _releaseTime
+        uint256 _nftReleaseTime,
+        address _nftContract
     ) internal {
         Beneficiary storage beneficiary = beneficiaries[index];
 
-        // Add ETH to current allocation
+        // ETH allocation
         beneficiary.ethAmount += _ethAmount;
+        beneficiary.ethReleaseTime = _ethReleaseTime;
         if (_ethAmount > 0) {
             require(msg.value == _ethAmount, "Incorrect ETH amount sent");
         }
 
-        // Add ERC20 tokens to current allocation if it's the same token type
+        // Token allocation
         if (_tokenAmount > 0 && _erc20Token == beneficiary.erc20Token) {
             beneficiary.tokenAmount += _tokenAmount;
+            beneficiary.tokenReleaseTime = _tokenReleaseTime;
             IERC20(_erc20Token).transferFrom(msg.sender, address(this), _tokenAmount);
         } else if (_tokenAmount > 0 && _erc20Token != beneficiary.erc20Token) {
-            // If different ERC20 token, update the allocation with new token type and amount
             beneficiary.tokenAmount = _tokenAmount;
             beneficiary.erc20Token = _erc20Token;
+            beneficiary.tokenReleaseTime = _tokenReleaseTime;
             IERC20(_erc20Token).transferFrom(msg.sender, address(this), _tokenAmount);
         }
 
-        // Update NFT allocation if a new NFT is specified
+        // NFT allocation
         if (_nftContract != address(0) && _nftTokenId > 0 && (_nftTokenId != beneficiary.nftTokenId || _nftContract != beneficiary.nftContract)) {
             beneficiary.nftTokenId = _nftTokenId;
             beneficiary.nftContract = _nftContract;
+            beneficiary.nftReleaseTime = _nftReleaseTime;
             IERC721(_nftContract).transferFrom(msg.sender, address(this), _nftTokenId);
         }
+    }
 
-        // Update release time if specified
-        if (_releaseTime > 0) {
-            beneficiary.releaseTime = _releaseTime;
+    // Claim inheritance for beneficiaries after the owner is confirmed dead
+    // Everyone can call this function to claim for the beneficiary
+    function claimInheritance() public onlyIfConfirmedDead {
+        uint256 index = beneficiaryIndex[msg.sender];
+        Beneficiary storage beneficiary = beneficiaries[index - 1];
+        uint256 currentTime = block.timestamp;
+
+        // Transfer ETH if release time is met
+        if (beneficiary.ethAmount > 0 && currentTime >= beneficiary.ethReleaseTime) {
+            require(address(this).balance >= beneficiary.ethAmount, "Insufficient contract balance");
+            payable(beneficiary.addr).transfer(beneficiary.ethAmount);
+            beneficiary.ethAmount = 0;
+        }
+
+        // Transfer ERC20 tokens if release time is met
+        if (beneficiary.tokenAmount > 0 && currentTime >= beneficiary.tokenReleaseTime && beneficiary.erc20Token != address(0)) {
+            IERC20(beneficiary.erc20Token).transfer(beneficiary.addr, beneficiary.tokenAmount);
+            beneficiary.tokenAmount = 0;
+        }
+
+        // Transfer NFT if release time is met
+        if (beneficiary.nftContract != address(0) && beneficiary.nftTokenId > 0 && currentTime >= beneficiary.nftReleaseTime) {
+            IERC721(beneficiary.nftContract).transferFrom(address(this), beneficiary.addr, beneficiary.nftTokenId);
+            beneficiary.nftTokenId = 0;
         }
     }
 
-    // Beneficiaries can claim assets if the owner is confirmed dead
-    // Assets will be transferred directly to the beneficiary
-    // Everyone can call this function, but assets will only be transferred to the beneficiaries
-    function claim() public onlyIfConfirmedDead {
-        for (uint256 i = 0; i < beneficiaries.length; i++) {
-            Beneficiary storage beneficiary = beneficiaries[i];
-            if (block.timestamp >= beneficiary.releaseTime) {
-                // Transfer ETH
-                if (beneficiary.ethAmount > 0 && address(this).balance >= beneficiary.ethAmount) {
-                    payable(beneficiary.addr).transfer(beneficiary.ethAmount);
-                    beneficiary.ethAmount = 0;
-                }
+    // Function for beneficiaries to transfer part of their allocated assets to a specified address
+    function transferAssets(
+        address to, 
+        uint256 ethAmount, 
+        uint256 tokenAmount, 
+        uint8 category
+    ) public onlyBeneficiary {
+        uint256 index = beneficiaryIndex[msg.sender];
+        Beneficiary storage beneficiary = beneficiaries[index - 1];
 
-                // Transfer ERC20 tokens
-                if (beneficiary.tokenAmount > 0 && beneficiary.erc20Token != address(0)) {
-                    IERC20(beneficiary.erc20Token).transfer(beneficiary.addr, beneficiary.tokenAmount);
-                    beneficiary.tokenAmount = 0;
-                }
+        require(to != address(0), "Invalid recipient address");
 
-                // Transfer NFT if specified
-                if (beneficiary.nftContract != address(0) && beneficiary.nftTokenId > 0) {
-                    IERC721(beneficiary.nftContract).transferFrom(address(this), beneficiary.addr, beneficiary.nftTokenId);
-                    beneficiary.nftTokenId = 0;
-                }
-            }
+        // Check the category condition
+        require(categoryCheck(category), "Category check failed");
+
+        // ETH Transfer
+        if (ethAmount > 0) {
+            require(ethAmount <= beneficiary.ethAmount, "Amount exceeds allocated ETH");
+            beneficiary.ethAmount -= ethAmount;
+            require(address(this).balance >= ethAmount, "Insufficient contract balance");
+            payable(to).transfer(ethAmount);
+        }
+
+        // ERC20 Token Transfer
+        if (tokenAmount > 0) {
+            require(tokenAmount <= beneficiary.tokenAmount, "Amount exceeds allocated tokens");
+            beneficiary.tokenAmount -= tokenAmount;
+            IERC20(beneficiary.erc20Token).transfer(to, tokenAmount);
         }
     }
 
-    // Allows the contract to receive ether directly
+    // Example of a category check function
+    // NOT IMPLEMENTED YET
+    function categoryCheck(uint8 category) internal pure returns (bool) {
+        return true; 
+    }
+
     receive() external payable {}
 }
-
